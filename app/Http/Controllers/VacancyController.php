2127246e4469dd019d9f9f9448a4fd633681db75
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-05-16 22:40:48 +0700

    loker

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -24,7 +24,8 @@ class VacancyController extends Controller
         //     'lokers' => Vacancy::latest()->filter(request(['search']))->paginate(6)->withQueryString()
         // ]);
         // $lokers = Vacancy::latest()->filter(request(['search']))->paginate(6)->withQueryString();
-        $lokers = Vacancy::select('vacancies.id', 'vacancies.company_id', 'vacancies.posisi', 'vacancies.insentif', 'vacancies.min_pengalaman', 'vacancies.jobdesc', 'vacancies.kriteria', 'vacancies.link_pendaftaran', 'vacancies.domisili', 'vacancies.insentif', 'vacancies.created_at', 'vacancies.updated_at', 'companies.nama_perusahaan')->join('companies', 'companies.id', '=', 'vacancies.company_id')->orderBy('vacancies.created_at', 'DESC')
+        $lokers = Vacancy::select('vacancies.id', 'vacancies.company_id', 'vacancies.posisi', 'vacancies.insentif', 'vacancies.min_pengalaman', 'vacancies.jobdesc', 'vacancies.kriteria', 'vacancies.link_pendaftaran', 'vacancies.domisili', 'vacancies.insentif', 'vacancies.created_at', 'vacancies.updated_at', 'companies.nama_perusahaan', 'users.foto_profil')->join('companies', 'companies.id', '=', 'vacancies.company_id')->join('users', 'users.company_id', '=', 'companies.id')
+        ->orderBy('vacancies.created_at', 'DESC')
         ->filter(request(['search']))->paginate(6);
         return response()->json($lokers, 200);
     }
