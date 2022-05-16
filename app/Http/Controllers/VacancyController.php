ac76f1212335c5e81b235ebe2a501f5f3b10a4f2
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-05-17 00:11:53 +0700

    loker

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -30,14 +30,14 @@ class VacancyController extends Controller
         ->filter(request(['search']))->paginate(6);
 
         if($lokers->lastPage() > 1){
-            $getLokers = ['current_page' => $lokers->currentPage(), 'data' => $lokers->unique('pertanyaan_id'), 'next_page_url' => 'http://127.0.0.1:8000/api/forum/timeline?'.($lokers->currentPage()+1)];
+            $getLokers = ['current_page' => $lokers->currentPage(), 'data' => $lokers->unique('id'), 'next_page_url' => 'http://127.0.0.1:8000/api/forum/timeline?'.($lokers->currentPage()+1)];
         }else{
-            $getLokers = ['current_page' => $lokers->currentPage(), 'data' => $lokers->unique('pertanyaan_id'), 'next_page_url' => NULL];
+            $getLokers = ['current_page' => $lokers->currentPage(), 'data' => $lokers->unique('id'), 'next_page_url' => NULL];
         }
         if($lokers->lastPage() > 1){
-            $getLokers = ['current_page' => $lokers->currentPage(), 'data' => $lokers->unique('pertanyaan_id'), 'next_page_url' => 'http://127.0.0.1:8000/api/forum/timeline?'.($lokers->currentPage()+1)];
+            $getLokers = ['current_page' => $lokers->currentPage(), 'data' => $lokers->unique('id'), 'next_page_url' => 'https://apiwfl.herokuapp.com/api/loker?'.($lokers->currentPage()+1)];
         }else{
-            $getLokers = ['current_page' => $lokers->currentPage(), 'data' => $lokers->unique('pertanyaan_id'), 'next_page_url' => NULL];
+            $getLokers = ['current_page' => $lokers->currentPage(), 'data' => $lokers->unique('id'), 'next_page_url' => NULL];
         }
         return response()->json($getLokers, 200);
     }
