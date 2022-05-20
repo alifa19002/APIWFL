d957e2d9deb0f6a19f79c6afefcc472f7baf8671
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-05-20 14:55:43 +0700

    solve

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -18,20 +18,28 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'nama' => 'required|max:255',
-            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
-            'email' => 'required|email:dns|unique:users',
-            'no_telp' => 'required|numeric|digits_between:10,14',
-            'jk' => 'required|max:1',
-            'password' => 'required|min:5|max:255',
-            'role' => 'required',
-        ]);
+        // $validatedData = $request->validate([
+        //     'nama' => 'required|max:255',
+        //     'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+        //     'email' => 'required|email:dns|unique:users',
+        //     'no_telp' => 'required|numeric|digits_between:10,14',
+        //     'jk' => 'required|max:1',
+        //     'password' => 'required|min:5|max:255',
+        //     'role' => 'required',
+        // ]);
 
         // $validatedData['password'] = bcrypt($validatedData['password']);
-        $validatedData['password'] = Hash::make($validatedData['password']);
+        // $validatedData['password'] = Hash::make($validatedData['password']);
 
-        $register = User::create($validatedData);
+        $register = User::create([
+            'nama' => $request->input('nama'),
+            'username' => $request->input('username'),
+            'email' => $request->input('email'),
+            'no_telp' => $request->input('no_telp'),
+            'password' => $request->input('password'),
+            'role' => $request->input('role'),
+            'jk' => $request->input('jk')
+        ]);
         // $request->session()->flash('success', 'Registration successful, please login!');
         // return redirect('/login')->with('success', 'Registration successful, please login!');
         if ($register) {
