ffb8b50afa2030b69f28eea54b0d87497e943fa2
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-12-03 17:07:50 +0700

    export for netlify

---
+++ dist/js/dropdown.js
@@ -0,0 +1,24 @@
+// set the dropdown menu element
+const targetEl = document.getElementById('dropdownMenu');
+
+// set the element that trigger the dropdown menu on click
+const triggerEl = document.getElementById('dropdownButton');
+
+// options with default values
+const options = {
+  placement: 'bottom',
+  onHide: () => {
+      console.log('dropdown has been hidden');
+  },
+  onShow: () => {
+      console.log('dropdown has been shown');
+  }
+};
+
+const dropdown = new Dropdown(targetEl, triggerEl, options);
+
+// show the dropdown menu
+dropdown.show();
+
+// hide the dropdown menu
+dropdown.hide();
